c6caabda42f3487264e70dd5d90eeca1dd0a2e4b
Dipak Acharya <dpakach@gmail.com>
2021-09-03 11:12:16 +0545

    Replace the basic unit tests with snapshot tests in roleItem component

--- packages/web-app-files/tests/unit/components/SideBar/Shared/RoleItem.spec.js
+++ packages/web-app-files/tests/unit/components/SideBar/Shared/RoleItem.spec.js
@@ -4,12 +4,6 @@ import RoleItem from '@files/src/components/SideBar/Shared/RoleItem'
 
 const localVue = createLocalVue()
 
-const selectors = {
-  roleItem: '.roles-select-role-item',
-  roleLabel: '.roles-select-role-item-label',
-  roleDescription: '.roles-select-role-item-description'
-}
-
 const filesPersonalRoute = { name: 'files-personal' }
 
 function getWrapper(route, role) {
@@ -33,20 +27,7 @@ describe('RoleItem', () => {
   }
   const wrapper = getWrapper(filesPersonalRoute, role)
 
-  it('should show the roleItem', () => {
-    const roleItem = wrapper.find(selectors.roleItem)
-    expect(roleItem.exists()).toBeTruthy()
-  })
-
-  it('should show role label', () => {
-    const roleLabel = wrapper.find(selectors.roleLabel)
-    expect(roleLabel.exists()).toBeTruthy()
-    expect(roleLabel.text()).toBe('Viewer')
-  })
-
-  it('should show role description', () => {
-    const roleDescription = wrapper.find(selectors.roleDescription)
-    expect(roleDescription.exists()).toBeTruthy()
-    expect(roleDescription.text()).toBe('Download, Preview and Share')
+  it('should display the roleItem correctly', () => {
+    expect(wrapper).toMatchSnapshot()
   })
 })
